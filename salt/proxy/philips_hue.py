2953fcc86371c2f5b4baa8c52b2c4b3d08e8fd81
Bo Maryniuk <bo@suse.de>
2015-10-14 12:10:05 +0200

    Doc fixes and new methods

--- salt/proxy/philips_hue.py
+++ salt/proxy/philips_hue.py
@@ -22,7 +22,14 @@ from __future__ import absolute_import
 
 # Import python libs
 import logging
-import requests
+
+# INFO: This is going to be removed anytime soon in favor of salt.utils.http
+#       But it needs a separate PR!
+try:
+    import requests
+except ImportError as err:
+    requests = None
+
 import time
 import json
 from salt.exceptions import (CommandExecutionError, MinionError)
@@ -30,31 +37,33 @@ from salt.exceptions import (CommandExecutionError, MinionError)
 
 __proxyenabled__ = ['philips_hue']
 
-GRAINS_CACHE = {}
 CONFIG = {}
 log = logging.getLogger(__file__)
 
 
-class Const:
+class Const(object):
     '''
     Constants for the lamp operations.
     '''
     LAMP_ON = {"on": True, "transitiontime": 0}
     LAMP_OFF = {"on": False, "transitiontime": 0}
 
-    COLOR_WHITE = {"sat": 0}
+    COLOR_WHITE = {"xy": [0.3227, 0.329]}
+    COLOR_DAYLIGHT = {"xy": [0.3806, 0.3576]}
     COLOR_RED = {"hue": 0, "sat": 254}
     COLOR_GREEN = {"hue": 25500, "sat": 254}
     COLOR_ORANGE = {"hue": 12000, "sat": 254}
-    COLOR_PINK = {"hue": 12000, "sat": 254}
+    COLOR_PINK = {"xy": [0.3688, 0.2095]}
     COLOR_BLUE = {"hue": 46920, "sat": 254}
+    COLOR_YELLOW = {"xy": [0.4432, 0.5154]}
+    COLOR_PURPLE = {"xy": [0.3787, 0.1724]}
 
 
 def __virtual__():
     '''
     Validate the module.
     '''
-    return True
+    return requests is not None
 
 
 def init(cnf):
@@ -70,24 +79,6 @@ def init(cnf):
         raise MinionError(message="Cannot find 'user' parameter in the proxy configuration")
 
     CONFIG['url'] = "http://{0}/api/{1}".format(host, user)
-
-
-def grains():
-    '''
-    Get the grains from the proxied device
-    '''
-    return grains_refresh()
-
-
-def grains_refresh():
-    '''
-    Refresh the grains from the proxied device
-    '''
-    if not GRAINS_CACHE:
-        GRAINS_CACHE['vendor'] = 'Philips'
-        GRAINS_CACHE['product'] = 'Hue Lamps'
-        
-    return GRAINS_CACHE
 
 
 def ping(*args, **kw):
@@ -115,7 +106,12 @@ def _set(lamp_id, state, method="state"):
     :return:
     '''
     url = "{0}/lights/{1}".format(CONFIG['url'], lamp_id) + (method and "/{0}".format(method) or '')
-    res = json.loads(requests.put(url, json=state).content)
+    res = None
+    try:
+        res = json.loads(requests.put(url, json=state).content)
+    except Exception as err:
+        raise CommandExecutionError(err)
+
     res = len(res) > 1 and res[-1] or res[0]
     if res.get('success'):
         res = {'result': True}
@@ -147,20 +143,34 @@ def _get_lights():
 
     :return:
     '''
-    return json.loads(requests.get(CONFIG['url'] + "/lights").content)
+    try:
+        return json.loads(requests.get(CONFIG['url'] + "/lights").content)
+    except Exception as exc:
+        raise CommandExecutionError(exc)
 
 
 # Callers
 def call_lights(*args, **kwargs):
     '''
-    Get info about available lamps.
+    Get info about all available lamps.
+
+    Options:
+
+    * **id**: Specifies a device ID. Can be a comma-separated values. All, if omitted.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' hue.lights
+        salt '*' hue.lights id=1
+        salt '*' hue.lights id=1,2,3
     '''
     res = dict()
     lights = _get_lights()
-    if 'id' in kwargs:
-        for dev_id in _get_devices(kwargs):
-            if lights.get(str(dev_id)):
-                res[dev_id] = lights[str(dev_id)]
+    for dev_id in 'id' in kwargs and _get_devices(kwargs) or sorted(lights.keys()):
+        if lights.get(str(dev_id)):
+            res[dev_id] = lights[str(dev_id)]
 
     return res or False
 
@@ -187,7 +197,7 @@ def call_switch(*args, **kwargs):
     '''
     out = dict()
     devices = _get_lights()
-    for dev_id in ('id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs)):
+    for dev_id in 'id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs):
         if 'on' in kwargs:
             state = kwargs['on'] and Const.LAMP_ON or Const.LAMP_OFF
         else:
@@ -207,7 +217,7 @@ def call_blink(*args, **kwargs):
     * **id**: Specifies a device ID. Can be a comma-separated values. All, if omitted.
     * **pause**: Time in seconds. Can be less than 1, i.e. 0.7, 0.5 sec.
 
-    CLE Example:
+    CLI Example:
 
     .. code-block:: bash
 
@@ -217,7 +227,7 @@ def call_blink(*args, **kwargs):
     devices = _get_lights()
     pause = kwargs.get('pause', 0)
     res = dict()
-    for dev_id in ('id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs)):
+    for dev_id in 'id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs):
         state = devices[str(dev_id)]['state']['on']
         _set(dev_id, state and Const.LAMP_OFF or Const.LAMP_ON)
         if pause:
@@ -229,7 +239,13 @@ def call_blink(*args, **kwargs):
 
 def call_ping(*args, **kwargs):
     '''
-    Ping the lamps
+    Ping the lamps by issuing a short inversion blink to all available devices.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' hue.ping
     '''
     errors = dict()
     for dev_id, dev_status in call_blink().items():
@@ -242,10 +258,22 @@ def call_ping(*args, **kwargs):
 def call_status(*args, **kwargs):
     '''
     Return the status of the lamps.
+
+    Options:
+
+    * **id**: Specifies a device ID. Can be a comma-separated values. All, if omitted.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' hue.status
+        salt '*' hue.status id=1
+        salt '*' hue.status id=1,2,3
     '''
     res = dict()
-    devices = _get_devices()
-    for dev_id in devices:
+    devices = _get_lights()
+    for dev_id in 'id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs):
         res[dev_id] = {
             'on': devices[dev_id]['state']['on'],
             'reachable': devices[dev_id]['state']['reachable']
@@ -263,7 +291,7 @@ def call_rename(*args, **kwargs):
     * **id**: Specifies a device ID. Only one device at a time.
     * **title**: Title of the device.
 
-    CLE Example:
+    CLI Example:
 
     .. code-block:: bash
 
@@ -285,10 +313,10 @@ def call_alert(*args, **kwargs):
 
     Options:
 
-    * **id**: Specifies a device ID. Can be comma-separated ids or all, if omitted.
+    * **id**: Specifies a device ID. Can be a comma-separated values. All, if omitted.
     * **on**: Turns on or off an alert. Default is True.
 
-    CLE Example:
+    CLI Example:
 
     .. code-block:: bash
 
@@ -299,7 +327,7 @@ def call_alert(*args, **kwargs):
     res = dict()
 
     devices = _get_lights()
-    for dev_id in ('id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs)):
+    for dev_id in 'id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs):
         res[dev_id] = _set(dev_id, {"alert": kwargs.get("on", True) and "lselect" or "none"})
 
     return res
@@ -311,21 +339,21 @@ def call_effect(*args, **kwargs):
 
     Options:
 
-    * **id**: Specifies a device ID. Can be comma-separated ids or all, if omitted.
+    * **id**: Specifies a device ID. Can be a comma-separated values. All, if omitted.
     * **type**: Type of the effect. Possible values are "none" or "colorloop". Default "none".
 
-    CLE Example:
+    CLI Example:
 
     .. code-block:: bash
 
-        salt '*' hue.alert
-        salt '*' hue.alert id=1
-        salt '*' hue.alert id=1,2,3 on=false
+        salt '*' hue.effect
+        salt '*' hue.effect id=1
+        salt '*' hue.effect id=1,2,3 type=colorloop
     '''
     res = dict()
 
     devices = _get_lights()
-    for dev_id in ('id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs)):
+    for dev_id in 'id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs):
         res[dev_id] = _set(dev_id, {"effect": kwargs.get("type", "none")})
 
     return res
@@ -337,17 +365,24 @@ def call_color(*args, **kwargs):
 
     Options:
 
-    * **id**: Specifies a device ID. Can be comma-separated ids or all, if omitted.
-    * **color**: Fixed color. Values are: red, green, blue, orange, pink, white. Default white.
+    * **id**: Specifies a device ID. Can be a comma-separated values. All, if omitted.
+    * **color**: Fixed color. Values are: red, green, blue, orange, pink, white,
+                 yellow, daylight, purple. Default white.
     * **transition**: Transition 0~200.
 
-    CLE Example:
+    Advanced:
+
+    * **gamut**: XY coordinates. Use gamut according to the Philips HUE devices documentation.
+                 More: http://www.developers.meethue.com/documentation/hue-xy-values
+
+    CLI Example:
 
     .. code-block:: bash
 
         salt '*' hue.color
         salt '*' hue.color id=1
         salt '*' hue.color id=1,2,3 oolor=red transition=30
+        salt '*' hue.color id=1 gamut=0.3,0.5
     '''
     res = dict()
 
@@ -358,12 +393,107 @@ def call_color(*args, **kwargs):
         'orange': Const.COLOR_ORANGE,
         'pink': Const.COLOR_PINK,
         'white': Const.COLOR_WHITE,
+        'yellow': Const.COLOR_YELLOW,
+        'daylight': Const.COLOR_DAYLIGHT,
+        'purple': Const.COLOR_PURPLE,
     }
 
     devices = _get_lights()
-    for dev_id in ('id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs)):
+    color = kwargs.get("gamut")
+    if color:
+        color = color.split(",")
+        if len(color) == 2:
+            try:
+                color = {"xy": [float(color[0]), float(color[1])]}
+            except Exception as ex:
+                color = None
+        else:
+            color = None
+
+    if not color:
         color = colormap.get(kwargs.get("color", 'white'), Const.COLOR_WHITE)
-        color.update({"transitiontime": max(min(kwargs.get("transition", 0), 200), 0)})
+    color.update({"transitiontime": max(min(kwargs.get("transition", 0), 200), 0)})
+
+    for dev_id in 'id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs):
         res[dev_id] = _set(dev_id, color)
+
+    return res
+
+
+def call_brightness(*args, **kwargs):
+    '''
+    Set an effect to the lamp.
+
+    Arguments:
+
+    * **value**: 0~255 brightness of the lamp.
+
+    Options:
+
+    * **id**: Specifies a device ID. Can be a comma-separated values. All, if omitted.
+    * **transition**: Transition 0~200. Default 0.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' hue.brightness value=100
+        salt '*' hue.brightness id=1 value=150
+        salt '*' hue.brightness id=1,2,3 value=255
+    '''
+    res = dict()
+
+    if 'value' not in kwargs:
+        raise CommandExecutionError("Parameter 'value' is missing")
+
+    try:
+        brightness = max(min(int(kwargs['value']), 244), 1)
+    except Exception as err:
+        raise CommandExecutionError("Parameter 'value' does not contains an integer")
+
+    try:
+        transition = max(min(int(kwargs['transition']), 200), 0)
+    except Exception as err:
+        transition = 0
+
+    devices = _get_lights()
+    for dev_id in 'id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs):
+        res[dev_id] = _set(dev_id, {"bri": brightness, "transitiontime": transition})
+
+    return res
+
+
+def call_temperature(*args, **kwargs):
+    '''
+    Set the mired color temperature. More: http://en.wikipedia.org/wiki/Mired
+
+    Arguments:
+
+    * **value**: 150~500.
+
+    Options:
+
+    * **id**: Specifies a device ID. Can be a comma-separated values. All, if omitted.
+
+    CLI Example:
+
+    .. code-block:: bash
+
+        salt '*' hue.temperature value=150
+        salt '*' hue.temperature value=150 id=1
+        salt '*' hue.temperature value=150 id=1,2,3
+    '''
+    res = dict()
+
+    if 'value' not in kwargs:
+        raise CommandExecutionError("Parameter 'value' (150~500) is missing")
+    try:
+        value = max(min(int(kwargs['value']), 500), 150)
+    except Exception as err:
+        raise CommandExecutionError("Parameter 'value' does not contains an integer")
+
+    devices = _get_lights()
+    for dev_id in 'id' not in kwargs and sorted(devices.keys()) or _get_devices(kwargs):
+        res[dev_id] = _set(dev_id, {"ct": value})
 
     return res
